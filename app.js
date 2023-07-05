abd43cdf6b5a8a08d3908c19509fc2d0262c262b
Osman Can <111634974+OsmnCnG@users.noreply.github.com>
2023-07-05 19:01:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+function downloadCourseraCertificate() {
+    var link = document.createElement('a');
+    link.href = '/certificate/Coursera EXV7BLZYKAAY.pdf';
+    link.download = 'myCertificate';
+    link.click();
+}
+
+
+
+function downloadPatikaCertificate() {
+    var link = document.createElement('a');
+    link.href = '/certificate/javaPatika.pdf';
+    link.download = 'myCertificate';
+    link.click();
+}
+
+
+function downloadBorusanCertificate() {
+    var link = document.createElement('a');
+    link.href = '/certificate/Borusan-Teknoloji-Okulu-Sertifikasi_Borusan-Teknoloji-Okulu_Osman-Can-GONEN.pdf.pdf';
+    link.download = 'myCertificate';
+    link.click();
+}
